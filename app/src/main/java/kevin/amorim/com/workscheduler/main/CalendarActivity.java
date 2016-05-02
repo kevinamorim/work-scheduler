01eded7aa113b4ebc2fd838895cf0c5ff57097b6
Kevin Amorim <kevinamorim4@gmail.com>
2016-05-02 21:35:45 +0100

    Created CalendarActivity

---
+++ app/src/main/java/kevin/amorim/com/workscheduler/main/CalendarActivity.java
@@ -0,0 +1,15 @@
+package kevin.amorim.com.workscheduler.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kevin.amorim.com.workscheduler.R;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
